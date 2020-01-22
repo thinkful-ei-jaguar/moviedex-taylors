696773b7e56c03a411624234ba4070fd8d5af3e3
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-01-22 15:12:12 -0500

    created server, incorporated genre query

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const movies = require('./movies');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+const validGenres = ['Animation', 'Drama', 'Romantic', 'Comedy', 'Spy', 'Crime', 'Thriller', 'Adventure', 'Documentary', 'Horror', 'Action', 'Western', 'History', 'Biography', 'Musical', 'Fantasy', 'War', 'Grotesque'];
+
+app.get('/movie', (req, res) => {
+    const { genre, country, avg_vote } = req.query;
+    let response = movies;
+    
+    if (genre) {
+        response = response.filter(movie => movie.genre.toLocaleLowerCase().includes(genre.toLowerCase()));
+    }
+
+
+    res.json(response)
+})
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+  });
